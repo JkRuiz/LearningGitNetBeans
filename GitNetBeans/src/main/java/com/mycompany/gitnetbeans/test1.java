3240fb0cb94ff471d8c4f7dbf0ea8a7899070bf3
Juan Camilo Ruiz <jc.ruiz@uniandes.edu.co>
2018-06-21 16:27:29 -0500

    Creación de la clase Test1

---
+++ GitNetBeans/src/main/java/com/mycompany/gitnetbeans/test1.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gitnetbeans;
+
+/**
+ *
+ * @author JuanCamiloRuiz
+ */
+public class test1 {
+    
+    private String name;
+    
+    private Long id;
+    
+    private String description;
+
+    public test1(String name, Long id, String description) {
+        this.name = name;
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
