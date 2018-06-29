a122349b21d70e01b7fa04b2d21c19a26cad24ed
Juan Camilo <jc.ruiz@uniandes.edu.co>
2018-06-29 11:13:22 -0500

    Modificaciones test1

--- GitNetBeans/src/main/java/com/mycompany/gitnetbeans/test1.java
+++ GitNetBeans/src/main/java/com/mycompany/gitnetbeans/test1.java
@@ -13,6 +13,8 @@ public class test1 {
     
     private String name;
     
+    private String apellido;
+    
     private Long id;
     
     private String description;
@@ -24,11 +26,7 @@ public class test1 {
     }
 
     public String getName() {
-        return name;
-    }
-    
-    public void setName(String name){
-       this.name = name;
+        return apellido;
     }
 
     public Long getId() {
