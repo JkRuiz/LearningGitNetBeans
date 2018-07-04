f996a4c0596677cec5e8feb86f1615ba3aeaa6c8
Juan Camilo Ruiz <jc.ruiz@uniandes.edu.co>
2018-07-04 11:28:20 -0500

    cambios test2

--- GitNetBeans/src/main/java/com/mycompany/gitnetbeans/test2.java
+++ GitNetBeans/src/main/java/com/mycompany/gitnetbeans/test2.java
@@ -15,8 +15,9 @@ public class test2 {
     
     private int cant;
     
+    private double cost2;
     private double cost3;
-
+    
     public test2(String name, int cant, double cost) {
         this.name = name;
         this.cant = cant;
@@ -25,10 +26,6 @@ public class test2 {
 
     public double getCost() {
         return cost3;
-    }
-    
-    public double getRealCost(){
-        return cost3 * 100;
     }
 
     public void setCost(double cost) {
